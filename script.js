2d421a08448b3761c1afbaf6d121f609e616b193
aohanesian <103946712+aohanesian@users.noreply.github.com>
2024-04-21 20:05:51 +0200

    added buttons for app state

--- script.js
+++ script.js
@@ -1,25 +1,117 @@
+class Queue {
+    constructor() {
+        this.elements = [];
+    }
+
+    enqueue(element) {
+        element.classList.add('selected');
+        element.disabled = 'true';
+        this.elements.push(element);
+    }
+
+    dequeue() {
+        const dequeuedElement = this.elements.shift();
+        dequeuedElement.classList.remove('selected');
+        return dequeuedElement;
+    }
+
+
+    isEmpty() {
+        return this.elements.length === 0;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.elements = [];
+    }
+
+    push(element) {
+        element.classList.add('selected');
+        element.disabled = 'true';
+        this.elements.push(element);
+    }
+
+    pop() {
+        const poppedElement = this.elements.pop();
+        poppedElement.classList.remove('selected');
+        return poppedElement;
+    }
+
+    isEmpty() {
+        return this.elements.length === 0;
+    }
+}
+
 const TILES = document.querySelectorAll('.tile');
 const TILES_COUNT = TILES.length;
+let clickedTiles;
 
+function StackListener(event) {
+    if (!event.target.classList.contains('tile') || clickedTiles.elements.includes(event.target) || event.target.disabled === 'true') return;
 
-const clickedTiles = [];
+    clickedTiles.push(event.target);
 
-document.addEventListener('click', function (event) {
-    if (!event.target.classList.contains('tile') || clickedTiles.includes(event.target) || event.target.disabled === 'true') return;
-    
-    const currTile = event.target
-    clickedTiles.push(currTile);
-    currTile.classList.add('selected');
-    currTile.disabled = 'true';
-
-    if (clickedTiles.length >= TILES_COUNT) {
+    if (clickedTiles.elements.length >= TILES_COUNT) {
         const interval = setInterval(() => {
-            const tile = clickedTiles.shift();
-            tile.classList.remove('selected');
-            if (clickedTiles.length === 0) {
+            const tile = clickedTiles.pop();
+            if (clickedTiles.isEmpty()) {
                 clearInterval(interval);
                 TILES.forEach(tile => tile.disabled = 'false');
             };
         }, 330);
     };
-});
+};
+
+function QueueListener(event) {
+    if (!event.target.classList.contains('tile') || clickedTiles.elements.includes(event.target) || event.target.disabled === 'true') return;
+
+    clickedTiles.enqueue(event.target);
+
+    if (clickedTiles.elements.length >= TILES_COUNT) {
+        const interval = setInterval(() => {
+            const tile = clickedTiles.dequeue();
+            if (clickedTiles.isEmpty()) {
+                clearInterval(interval);
+                TILES.forEach(tile => tile.disabled = 'false');
+            };
+        }, 330);
+    };
+};
+
+function handleStackClick() {
+    clickedTiles = new Stack();
+    document.addEventListener('click', StackListener);
+}
+
+function handleQueueClick() {
+    clickedTiles = new Queue();
+    document.addEventListener('click', QueueListener);
+}
+
+
+function initialize() {
+    document.getElementById('stackButton').addEventListener('click', handleStackClick);
+    document.getElementById('queueButton').addEventListener('click', handleQueueClick);
+
+    function gridAlert() {
+        alert('Choose type of data structure');
+    }
+
+    const buttons = document.querySelectorAll('#stackButton, #queueButton');
+    const grid = document.querySelector('.grid');
+    grid.addEventListener('click', gridAlert)
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            grid.style.cursor = 'auto';
+            grid.removeEventListener('click', gridAlert);
+            buttons.forEach(btn => btn.disabled = 'true');
+        });
+    });
+};
+
+initialize();
+
+
+
